6bc116512e1b61cc3c64a16226c5b4c2102e96e8
Yağmur K. <yagmurrttk@gmail.com>
2024-05-22 11:45:13 +0300

    Add project files.

---
+++ BlogForest.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,23 @@
+﻿using BlogForest.EntityLayer;
+using BlogForest.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogForest.DataAccessLayer.Context
+{
+    public class BlogContext : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-C1IT2N4\\MSSQLSERVER02;initial Catalog= DbBlogyForest;integrated Security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ BlogForest.WebUI/ViewComponents/DefaultViewComponents/_TopbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogForest.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _TopbarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
